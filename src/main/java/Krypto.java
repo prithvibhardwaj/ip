99c3c0bfd1faf71e0251fa0e1bbf5454951d5c09
Prithvi Bhardwaj <prithvi.p.bhardwaj@gmail.com>
2026-01-20 12:41:02 +0800

    Level-0

---
+++ src/main/java/Krypto.java
@@ -0,0 +1,18 @@
+public class Krypto {
+    public static void main(String[] args) {
+    String logo = " _  __                  _       \n" +
+                  "| |/ /                 | |      \n" +
+                  "| ' / _ __ _   _ _ __ | |_ ___ \n" +
+                  "|  < | '__| | | | '_ \\| __/ _ \\\n" +
+                  "| . \\| |  | |_| | |_) | || (_) |\n" +
+                  "|_|\\_\\_|   \\__, | .__/ \\__\\___/\n" +
+                  "            __/ | |             \n" +
+                  "           |___/|_|             ";
+    System.out.println("Hello from\n" + logo);
+    System.out.println("____________________________________________________________");
+    System.out.println(" Hello! I'm Krypto\n What can I do for you?");
+    System.out.println("____________________________________________________________");
+    System.out.println(" Bye. Hope to see you again soon!");
+    System.out.println("____________________________________________________________");
+}
+}
