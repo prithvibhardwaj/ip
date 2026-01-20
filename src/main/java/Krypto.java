208e2bf392ba1e73dd33867a8e32aa87a113ae9d
Prithvi Bhardwaj <prithvi.p.bhardwaj@gmail.com>
2026-01-20 15:23:53 +0800

    Level-5

--- src/main/java/Krypto.java
+++ src/main/java/Krypto.java
@@ -25,44 +25,60 @@ public class Krypto {
 
         while (!command.equals("bye")) {
             System.out.println("____________________________________________________________");
-            if (command.equals("list")) {
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < count; i++) {
-                    System.out.println(" " + (i + 1) + "." + tasks[i]);
+            try {
+                if (command.equals("list")) {
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(" " + (i + 1) + "." + tasks[i]);
+                    }
+                } else if (command.startsWith("mark")) {
+                    int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                    tasks[index].markAsDone();
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks[index]);
+                } else if (command.startsWith("unmark")) {
+                    int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                    tasks[index].markAsUndone();
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + tasks[index]);
+                } else if (command.startsWith("todo")) {
+                    if (command.length() <= 5) {
+                        throw new KryptoException("OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    String description = command.substring(5);
+                    tasks[count] = new Todo(description);
+                    count++;
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + tasks[count - 1]);
+                    System.out.println(" Now you have " + count + " tasks in the list.");
+                } else if (command.startsWith("deadline")) {
+                    if (!command.contains(" /by ")) {
+                        throw new KryptoException("OOPS!!! Deadline must have a /by date.");
+                    }
+                    String[] parts = command.substring(9).split(" /by ");
+                    tasks[count] = new Deadline(parts[0], parts[1]);
+                    count++;
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + tasks[count - 1]);
+                    System.out.println(" Now you have " + count + " tasks in the list.");
+                } else if (command.startsWith("event")) {
+                    if (!command.contains(" /from ") || !command.contains(" /to ")) {
+                        throw new KryptoException("OOPS!!! Event must have /from and /to.");
+                    }
+                    String[] parts = command.substring(6).split(" /from ");
+                    String[] times = parts[1].split(" /to ");
+                    tasks[count] = new Event(parts[0], times[0], times[1]);
+                    count++;
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + tasks[count - 1]);
+                    System.out.println(" Now you have " + count + " tasks in the list.");
+                } else {
+                    throw new KryptoException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            } else if (command.startsWith("mark")) {
-                int index = Integer.parseInt(command.split(" ")[1]) - 1;
-                tasks[index].markAsDone();
-                System.out.println(" Nice! I've marked this task as done:");
-                System.out.println("   " + tasks[index]);
-            } else if (command.startsWith("unmark")) {
-                int index = Integer.parseInt(command.split(" ")[1]) - 1;
-                tasks[index].markAsUndone();
-                System.out.println(" OK, I've marked this task as not done yet:");
-                System.out.println("   " + tasks[index]);
-            } else if (command.startsWith("todo")) {
-                String description = command.substring(5);
-                tasks[count] = new Todo(description);
-                count++;
-                System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[count - 1]);
-                System.out.println(" Now you have " + count + " tasks in the list.");
-            } else if (command.startsWith("deadline")) {
-                String[] parts = command.substring(9).split(" /by ");
-                tasks[count] = new Deadline(parts[0], parts[1]);
-                count++;
-                System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[count - 1]);
-                System.out.println(" Now you have " + count + " tasks in the list.");
-            } else if (command.startsWith("event")) {
-                String[] parts = command.substring(6).split(" /from ");
-                String[] times = parts[1].split(" /to ");
-                tasks[count] = new Event(parts[0], times[0], times[1]);
-                count++;
-                System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks[count - 1]);
-                System.out.println(" Now you have " + count + " tasks in the list.");
+            } catch (KryptoException e) {
+                System.out.println(" " + e.getMessage());
             }
+
             System.out.println("____________________________________________________________");
             command = scanner.nextLine();
         }
@@ -70,6 +86,12 @@ public class Krypto {
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+    }
+}
+
+class KryptoException extends Exception {
+    public KryptoException(String message) {
+        super(message);
     }
 }
 
@@ -81,53 +103,30 @@ class Task {
         this.description = description;
         this.isDone = false;
     }
-    public String getStatusIcon() {
-        return (isDone ? "X" : " ");
-    }
-    public void markAsDone() {
-        this.isDone = true;
-    }
-    public void markAsUndone() {
-        this.isDone = false;
-    }
+    public String getStatusIcon() { return (isDone ? "X" : " "); }
+    public void markAsDone() { this.isDone = true; }
+    public void markAsUndone() { this.isDone = false; }
     @Override
-    public String toString() {
-        return "[" + getStatusIcon() + "] " + description;
-    }
+    public String toString() { return "[" + getStatusIcon() + "] " + description; }
 }
 
 class Todo extends Task {
-    public Todo(String description) {
-        super(description);
-    }
+    public Todo(String description) { super(description); }
     @Override
-    public String toString() {
-        return "[T]" + super.toString();
-    }
+    public String toString() { return "[T]" + super.toString(); }
 }
 
 class Deadline extends Task {
     protected String by;
-    public Deadline(String description, String by) {
-        super(description);
-        this.by = by;
-    }
+    public Deadline(String description, String by) { super(description); this.by = by; }
     @Override
-    public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
-    }
+    public String toString() { return "[D]" + super.toString() + " (by: " + by + ")"; }
 }
 
 class Event extends Task {
     protected String from;
     protected String to;
-    public Event(String description, String from, String to) {
-        super(description);
-        this.from = from;
-        this.to = to;
-    }
+    public Event(String description, String from, String to) { super(description); this.from = from; this.to = to; }
     @Override
-    public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
-    }
+    public String toString() { return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")"; }
 }
