7f98d8baac8af0baa283882495f28f1ef0cf6920
Prithvi Bhardwaj <prithvi.p.bhardwaj@gmail.com>
2026-01-20 15:06:39 +0800

    Level-4

--- src/main/java/Krypto.java
+++ src/main/java/Krypto.java
@@ -40,10 +40,28 @@ public class Krypto {
                 tasks[index].markAsUndone();
                 System.out.println(" OK, I've marked this task as not done yet:");
                 System.out.println("   " + tasks[index]);
-            } else {
-                tasks[count] = new Task(command);
+            } else if (command.startsWith("todo")) {
+                String description = command.substring(5);
+                tasks[count] = new Todo(description);
                 count++;
-                System.out.println(" added: " + command);
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[count - 1]);
+                System.out.println(" Now you have " + count + " tasks in the list.");
+            } else if (command.startsWith("deadline")) {
+                String[] parts = command.substring(9).split(" /by ");
+                tasks[count] = new Deadline(parts[0], parts[1]);
+                count++;
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[count - 1]);
+                System.out.println(" Now you have " + count + " tasks in the list.");
+            } else if (command.startsWith("event")) {
+                String[] parts = command.substring(6).split(" /from ");
+                String[] times = parts[1].split(" /to ");
+                tasks[count] = new Event(parts[0], times[0], times[1]);
+                count++;
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[count - 1]);
+                System.out.println(" Now you have " + count + " tasks in the list.");
             }
             System.out.println("____________________________________________________________");
             command = scanner.nextLine();
@@ -63,21 +81,53 @@ class Task {
         this.description = description;
         this.isDone = false;
     }
-
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " ");
     }
-
     public void markAsDone() {
         this.isDone = true;
     }
-
     public void markAsUndone() {
         this.isDone = false;
     }
-
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
+    }
+}
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
