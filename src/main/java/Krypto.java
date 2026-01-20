f56a4f12d6e17d18d9da7831d29f3d2ff9678afd
Prithvi Bhardwaj <prithvi.p.bhardwaj@gmail.com>
2026-01-20 12:46:59 +0800

    Level-1

--- src/main/java/Krypto.java
+++ src/main/java/Krypto.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
+
 public class Krypto {
     public static void main(String[] args) {
-    String logo = " _  __                  _       \n" +
-                  "| |/ /                 | |      \n" +
-                  "| ' / _ __ _   _ _ __ | |_ ___ \n" +
-                  "|  < | '__| | | | '_ \\| __/ _ \\\n" +
-                  "| . \\| |  | |_| | |_) | || (_) |\n" +
-                  "|_|\\_\\_|   \\__, | .__/ \\__\\___/\n" +
-                  "            __/ | |             \n" +
-                  "           |___/|_|             ";
-    System.out.println("Hello from\n" + logo);
-    System.out.println("____________________________________________________________");
-    System.out.println(" Hello! I'm Krypto\n What can I do for you?");
-    System.out.println("____________________________________________________________");
-    System.out.println(" Bye. Hope to see you again soon!");
-    System.out.println("____________________________________________________________");
-}
+        String logo = " _  __                  _       \n" +
+                      "| |/ /                 | |      \n" +
+                      "| ' / _ __ _   _ _ __ | |_ ___ \n" +
+                      "|  < | '__| | | | '_ \\| __/ _ \\\n" +
+                      "| . \\| |  | |_| | |_) | || (_) |\n" +
+                      "|_|\\_\\_|   \\__, | .__/ \\__\\___/\n" +
+                      "            __/ | |             \n" +
+                      "           |___/|_|             ";
+
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Krypto\n What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + command);
+            System.out.println("____________________________________________________________");
+            command = scanner.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
 }
